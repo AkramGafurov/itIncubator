449a4f21631c032372a98c78c5e4970548058e3b
Akram Gafurov <akramminsk@yandex.by>
2022-07-15 20:51:31 +0300

    feat: do lesson 06. index.js, App, JSX

---
+++ wayOfSamurai/react-app-01/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+ const App =  () => {
+  return (
+    <div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, soluta!</div>
+      <p>Lorem ipsum dolor.</p>
+    </div>
+
+  );
+}
+
+export default App;
