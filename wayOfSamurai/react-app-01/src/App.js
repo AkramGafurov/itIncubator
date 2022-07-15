8ff1ea12c01710e59230e079ca8304fdc2ebd4a8
Akram Gafurov <akramminsk@yandex.by>
2022-07-15 21:55:04 +0300

    feat: do lesson 07. React components

--- wayOfSamurai/react-app-01/src/App.js
+++ wayOfSamurai/react-app-01/src/App.js
@@ -4,11 +4,22 @@ import './App.css';
  const App =  () => {
   return (
     <div>
+      <div><Header/></div>
       <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, soluta!</div>
       <p>Lorem ipsum dolor.</p>
     </div>
 
   );
 }
+const Header = () => {
+   return(
+     <div>
+       <a>Home</a>
+       <a>New feed</a>
+       <a>Messages</a>
+     </div>
+    )
+}
 
+// export default Header;
 export default App;
