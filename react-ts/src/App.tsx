8ceff15bc51147eea1125de999cf1e2b73eed374
Akram Gafurov <akramminsk@yandex.by>
2022-07-16 13:46:16 +0300

    feat: 02-react kabzda kak prosto (components part 2)

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -4,9 +4,10 @@ import './App.css';
 
 function App() {
   return (
-    <div>This is APP component
+    <div>
+        <TitleApp />
         <Rating />
-        <Acordion />
+        <Accordion />
     </div>
   );
 }
@@ -19,27 +20,40 @@ function  Star(){
 
 function Rating() {
   return (
-      <div>
+      <>
           <Star/>
           <Star/>
           <Star/>
           <Star/>
           <Star/>
-      </div>
+      </>
   );
 }
-
-function  Acordion(){
+function  Accordion(){
   return (
       <div>
-        <h3>menu</h3>
-          <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-          </ul>
+          <AccordionTitle/>
+          <AccordionBody/>
       </div>
   )
 }
+function TitleApp() {
+    return <>This is APP component</>
+}
+function AccordionTitle(){
+    return(
+        <h3>menu</h3>
+    )
+}
+function AccordionBody(){
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
 
 export default App;
+
