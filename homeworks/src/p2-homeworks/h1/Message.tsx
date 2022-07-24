d43118d786286c11e31faf970df3c287110a08ba
Akram Gafurov <akramminsk@yandex.by>
2022-07-25 01:11:54 +0300

    feat: add message component, add css to message component

--- homeworks/src/p2-homeworks/h1/Message.tsx
+++ homeworks/src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import s from './Message.module.css'
+
 
 function Message() {
     return (
-        <div>
-
+        <div className={s.message}>
+            <img className={s.message__avatar}
+                 src="https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg"
+                 alt="userAvatar"/>
+            <div className={s.content}>
+                <div className={s.content__box}>
+                    <h2 className={s.content__userName}>Artem</h2>
+                    <p className={s.content__userMessage}>npm start нажимал?</p>
+                </div>
+                <p className={s.content__time}>20:00</p>
+            </div>
         </div>
     )
 }
