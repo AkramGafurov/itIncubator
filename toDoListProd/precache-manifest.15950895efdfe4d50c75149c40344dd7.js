55805fefe738dc1a568d2d0dd361f26d2d841db4
Akram Gafurov <akramminsk@yandex.by>
2022-07-26 01:55:31 +0300

    feat: add build to gh-page

---
+++ toDoListProd/precache-manifest.15950895efdfe4d50c75149c40344dd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a54c49730671f320f5bff4ff61af7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887da60a48becc0eceb2",
+    "url": "/static/css/main.ccf4ba5b.chunk.css"
+  },
+  {
+    "revision": "9cb9b538998eff278bb4",
+    "url": "/static/js/2.9d407cf8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9d407cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887da60a48becc0eceb2",
+    "url": "/static/js/main.cd73176b.chunk.js"
+  },
+  {
+    "revision": "ad2af91736b280adb865",
+    "url": "/static/js/runtime-main.f123d06a.js"
+  }
+]);
