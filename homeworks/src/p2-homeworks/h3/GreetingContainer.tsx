31375c9e2ff9abd7d52dff7ea35664e503d92a46
Akram Gafurov <akramminsk@yandex.by>
2022-08-27 20:51:59 +0300

    refactor: done hw3

--- homeworks/src/p2-homeworks/h3/GreetingContainer.tsx
+++ homeworks/src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, ChangeEvent} from 'react'
 import Greeting from './Greeting'
+import {UserType} from './HW3'
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+    users: Array<UserType> // need to fix any
+    addUserCallback: (name: string) => void// need to fix any
 }
 
 // более простой и понятный для новичков
@@ -11,18 +12,32 @@ type GreetingContainerPropsType = {
 
 // более современный и удобный для про :)
 // уровень локальной логики
+
 const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+    
+    const [name, setName] = useState<string>('') // need to fix any
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
+    const [error, setError] = useState<string>('') // need to fix any
+
+    // контроль измения Input на onChange
+    const setNameCallback = (event:ChangeEvent<HTMLInputElement>) => { // need to fix any
+        let userInput = event.currentTarget.value
+        setName(userInput) // need to fix        
     }
+
+    // контроль нажатия button add на onChange
     const addUser = () => {
-        alert(`Hello  !`) // need to fix
+        if(name == ''){
+            setError('Обязательно для заполнения');
+            return;
+        }
+        addUserCallback(name);
+        setError('');
+        setName('')
+        alert(`Hello  ${name}!`) // need to fix        
     }
 
-    const totalUsers = 0 // need to fix
+    const totalUsers = users.length // need to fix
 
     return (
         <Greeting
